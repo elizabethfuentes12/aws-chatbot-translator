07000f15d1be22eb7c983a7916fb83ad837c8ad9
Elizabeth Fuentes <eliaws@amazon.com>
2023-06-20 10:11:22 -0400

    chatbot

--- lambdas/chatbot-translate-polly/lambda_function.py
+++ lambdas/chatbot-translate-polly/lambda_function.py
@@ -1,179 +1,61 @@
-import boto3
-import os
-import time
-import logging
 import json
-import lex_utils as lex
-import internal_utils as utils
-
-import requests
-
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-
-region_name = os.environ.get('ENV_REGION_NAME')
-bucket_key = os.environ.get('BUCKET_KEY')
-bucket_name = os.environ.get('BUCKET_NAME')
-
-language_code = dict( [ ('es','spanish'), ('pt','portuguese'), ('it','italian'), ('fr','french'),('en','english') ] )
-language_en = dict( [ ('spanish', 'es'), ('portuguese', 'pt-PT'), ('italian', 'it'), ('french', 'fr'),('english', 'en') ] )
-to_polly_voice = dict( [ ('en', 'Amy'), ('es', 'Conchita'), ('fr', 'Chantal'), ('pt-PT', 'Cristiano'),('it', 'Giorgio') ] )
-to_polly_language = dict( [ ('en', 'en-US'), ('es', 'es-US'), ('fr', 'fr-FR'), ('pt-PT' , 'pt-BR'),('it', 'it-IT') ] )
-
+import lex_utils as lex_lib
+import boto3
 
 client_comprehend = boto3.client('comprehend')
 client_translate = boto3.client('translate')
-client_polly = boto3.client('polly')
-
-# Create an S3 client
-s3_client = boto3.client('s3')
-
-    
-def smart_chat(intent_request):
-    print("intent_request: ",intent_request )
-    
-    """
-    Performs dialog management and fulfillment for ordering flowers.
-    Beyond fulfillment, the implementation of this intent demonstrates the use of the elicitSlot dialog action
-    in slot validation and re-prompting.
-    """
-    
-    source = intent_request['invocationSource']
-    text_to_translate = intent_request['inputTranscript']
-    active_contexts =  lex.get_active_contexts(intent_request)
-    session_attributes = intent_request['sessionState']['sessionAttributes']
-    intent = intent_request['interpretations'][0]["intent"]
-    
-    text = intent_request["interpretations"][0]["intent"]["slots"]["text"]
-    language = intent_request["interpretations"][0]["intent"]["slots"]["language"]
-    trasnlate = intent_request["interpretations"][0]["intent"]["slots"]["trasnlate"]
 
 
-    print("\nSearch results for text_to_translate: " + text_to_translate + "\n")
+def detetct_language (text):
+    response = client_comprehend.detect_dominant_language(
+        Text=text
+    )
+    return response['Languages'][0]['LanguageCode']
 
-    if source == 'DialogCodeHook':
-        # Perform basic validation on the supplied input slots.
-        # Use the elicitSlot dialog action to re-prompt for the first violation detected.
-        
-        if language is None: 
-            try:
-                slotToElicit = "language"
-             
-                state = 'ReadyForFulfillment'
-                text = text_to_translate
-                response_dominant_language = client_comprehend.detect_dominant_language(
-                                Text= text_to_translate
-                            )
-                
-                dominant_language = language_code[response_dominant_language["Languages"][0]["LanguageCode"]]
-                confidence = "{:.2f}".format(response_dominant_language["Languages"][0]["Score"]*100)
-                
-                print("\nSearch results for query: " + dominant_language + "\n")
-                
-             
-                response_chat = f" The dominant language of the text is: {dominant_language}, with a the level of confidence: {confidence} , \n \n What language do you want to translate? ".format(dominant_language,confidence)
-                print(response_chat)
-             
-                intent = lex.set_slot("trasnlate", response_dominant_language["Languages"][0]["LanguageCode"], intent)
-                intent = lex.set_slot("text",text, intent)
-                print("Intent: ",intent)
-                
-                return lex.elicit_slot(
-                    active_contexts, session_attributes, intent,slotToElicit,[{'contentType': 'PlainText', 'content': response_chat}])
-            except:
-                slotToElicit = "text"
-                intent = lex.set_slot("text",None, intent)
-                response_chat = f" I don't recognize {text_to_translate} as a language, try a valid language (spanish, portuguese, italian, french, english) ".format(text_to_translate)
-                return lex.elicit_slot(
-                    active_contexts, session_attributes, intent,slotToElicit,[{'contentType': 'PlainText', 'content': response_chat}])
-               
-            
-        elif language is not None :
-            try: 
-                
-                targetLanguage = language_en[text_to_translate.lower()]
-                response = client_translate.translate_text( Text=text['value']["interpretedValue"], SourceLanguageCode=trasnlate['value']["interpretedValue"], TargetLanguageCode=targetLanguage)
-                text_done = response['TranslatedText']
-    
-    
-                response_chat = f" Here yo response: {text_done}".format(text_done)
-                
-                print(response_chat)
-                
-                languageCode = to_polly_language[targetLanguage] 
-                targetVoice = to_polly_voice[targetLanguage]
-                
-                response_polly = client_polly.start_speech_synthesis_task(
-                                Engine='standard',
-                                LanguageCode = languageCode,
-                                OutputFormat='mp3',
-                                OutputS3BucketName=bucket_name,
-                                OutputS3KeyPrefix=bucket_key,
-                                Text=text_done,
-                                VoiceId= targetVoice
-                                )
-                print(response_polly)
-                
-                s3_path = bucket_key + response_polly['SynthesisTask']['OutputUri'].split("/")[-1]  
-    
-                mp3_presigned_url = utils.create_presigned_url(bucket_name, s3_path, expiration=3600)
-                
-                
-                print("mp3_presigned_url: ",mp3_presigned_url)
-                print("s3_path: ",s3_path)
-                
-                url_short = utils.getShortUrl(mp3_presigned_url)
-                
-            
-                response_chat = f"Translation result: {text_done} \n \n \n \n Here you can hear the pronunciation .. wait a few seconds: {url_short}".format(text_done,url_short)
-                
-                print(response_chat)
-           
-                
-                return lex.elicit_intent(
-                    active_contexts, session_attributes, intent,[{'contentType': 'PlainText', 'content': response_chat}])
-                    
-            except:
-                slotToElicit = "language"
-                intent = lex.set_slot("language",None, intent)
-                response_chat = f" I don't recognize {text_to_translate} as a language, try a valid language (spanish, portuguese, italian, french, english) ".format(text_to_translate)
-                return lex.elicit_slot(
-                    active_contexts, session_attributes, intent,slotToElicit,[{'contentType': 'PlainText', 'content': response_chat}])
-            
-        else:
-            response_chat = f" Wrong value.. try again"
-        
-            return lex.elicit_intent(
-                    active_contexts, session_attributes, intent,[{'contentType': 'PlainText', 'content': response_chat}])
+def translate_text (text,language_in,language_out):
+    response = client_translate.translate_text(
+    Text=text,
+    SourceLanguageCode=language_in,
+    TargetLanguageCode=language_out,
+  
+)
+    return response['TranslatedText'] 
 
-
-def dispatch(intent_request):
-    """
-    Called when the user specifies an intent for this bot.
-    """
-
-    logger.debug('dispatch sessionId={}, intentName={}'.format(intent_request['sessionId'], intent_request['sessionState']["intent"]['name']))
-
-    interpretations = intent_request['interpretations']
-    intent_name = interpretations[0]['intent']['name']
-
-    # Dispatch to your bot's intent handlers
-    if intent_name == 'NewIntent':
-        return smart_chat(intent_request)
-
-    raise Exception('Intent with name ' + intent_name + ' not supported')
 
 def lambda_handler(event, context):
-    print("evento: ",event)
+    # TODO implement
+    print(event)
     
-    """
-    Route the incoming request based on intent.
-    The JSON body of the request is provided in the event slot.
-    """
-    # By default, treat the user request as coming from the America/New_York time zone.
-    os.environ['TZ'] = 'America/New_York'
-    time.tzset()
-    logger.debug('event.bot.name={}'.format(event['bot']['name']))
-
-    return dispatch(event)
+    interpretations = event['interpretations']
+    intent_name = interpretations[0]['intent']['name']
+    intent = lex_lib.get_intent(event)
+    active_contexts = lex_lib.get_active_contexts(event)
+    session_attributes = lex_lib.get_session_attributes(event)
+    previous_slot_to_elicit = session_attributes.get("previous_slot_to_elicit")
     
+    if intent_name == 'TranslateIntent':
+        print(intent_name)
+        language_out = lex_lib.get_slot("language_out",intent)
+        text_to_translate = lex_lib.get_slot("text_to_translate",intent)
+        
+        if language_out == None:
+            return lex_lib.delegate(active_contexts, session_attributes, intent)
+            
+        if (text_to_translate == None) and (previous_slot_to_elicit != "text_to_translate") :
+            response = "What text do you want to translate?"
+            messages =  [{'contentType': 'PlainText', 'content': response}]
+            return lex_lib.elicit_slot("text_to_translate", active_contexts, session_attributes, intent, messages)
+       
+        if previous_slot_to_elicit == "text_to_translate": 
+            text_to_translate = event["inputTranscript"]
+            language_in = detetct_language (text_to_translate)
+    
+        print(language_out,text_to_translate,language_in)
+        
+        text_ready = translate_text(text_to_translate,language_in,language_out)
+        response = f"The translate text is: {text_ready}. \nAnything else?"
+        messages =  [{'contentType': 'PlainText', 'content': response}]
+        print(lex_lib.elicit_intent(active_contexts, session_attributes, intent, messages))
+        
+        return lex_lib.elicit_intent(active_contexts, session_attributes, intent, messages)
+        
